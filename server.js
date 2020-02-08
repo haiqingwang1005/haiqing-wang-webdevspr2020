15d8763ddc332a3b7af3719ac05533da2ea5cd68
Haiqing Wang <wang.haiq@husky.neu.edu>
2020-02-07 16:24:45 -0800

    Refactor the folder structure

--- server.js
+++ server.js
@@ -1,16 +1,40 @@
-var express = require('express')
-var app = express()
+var express = require('express');
+var app = express();
 
-app.set('port', (process.env.PORT || 5000))
+app.set('port', (process.env.PORT || 5000));
 
-//app.get('/', function(req, res) {
-//    res.sendFile(__dirname + '/index.html');
+app.use('/public', express.static(__dirname + '/public'));
+
+app.get('/', function(req, res) {
+    res.sendFile(__dirname + '/welcome.html');
+});
+
+app.get('/welcome.html', function(req, res) {
+    res.sendFile(__dirname + '/welcome.html');
+});
+
+app.get('/login.html', function(req, res) {
+    res.sendFile(__dirname + '/login.html');
+});
+
+app.get('/signup.html', function(req, res) {
+    res.sendFile(__dirname + '/signup.html');
+});
+
+app.get('/profile.html', function(req, res) {
+    res.sendFile(__dirname + '/profile.html');
+});
+
+app.get('/post1.html', function(req, res) {
+    res.sendFile(__dirname + '/post1.html');
+});
+
+
+
+//app.get('/', function(request, response) {
+//  response.send('Hello World!')
 //});
-//app.use('/public', express.static(__dirname + '/public'))
-app.get('/', function(request, response) {
-  response.send('Hello World!')
-})
 
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
-})
+});
